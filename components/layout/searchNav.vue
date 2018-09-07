50426f1a69af8a08067b66bde38617af6ba36a80
Your Name <you@example.com>
2018-09-07 15:37:22 +0800

    a

---
+++ components/layout/searchNav.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="nav">
+        <div class="container">
+            <div class="homeHead">
+                <div class="left"><nuxt-link to="/"><img src="~static/images/thmart.png" alt=""></nuxt-link></div>
+                <div class="center">
+                    <i v-show="searchTipsTextShow" class="iconfont icon-sousuo"> What are you looking for?</i>
+                    <input type="search" @focus="handleinput()" value="" name="" id="">
+                    <button>Search</button>
+                </div>
+                <div class="right"><img src="~static/images/thmartCode.jpg" alt=""></div>
+            </div> 
+        </div> 
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                searchText: '',
+                searchTipsTextShow: true
+            }
+        },
+        methods: {
+            handleinput() {
+                this.searchTipsTextShow = false
+            },
+        }
+    }
+</script>
+
+<style lang='sass' type="text/css" scoped>
+    @import '~/assets/sass/common.sass'
+    .homeHead
+        margin: 15px 0
+        overflow: hidden
+        @include hh(80px, 80px)
+        >div
+            float: left
+        .left, .right
+            img
+                vertical-align: middle
+        .left
+            img
+                @include wh(80px, 60px)
+        .right
+            float: right
+            img
+                @include wh(80px, 80px)
+        .center
+            position: relative
+            margin-left: 23%
+            @include hh(80px, 80px)
+            i
+                position: absolute
+                color: #999
+                left: 13px
+                top: 3px
+                z-index: -1
+            input
+                @include whh(380px, 40px, 40px)
+                color: $describe_color
+                @include ttbb(4px, 0, 4px, 0) 
+                padding: 0 10px 
+                background: transparent  
+                box-sizing: border-box
+            button
+                background-color: $theme_color
+                @include sc(16px, #fff)
+                @include whh(120px, 40px, 40px)
+                @include ttbb(0, 4px, 0, 4px)
+                cursor: pointer
+                vertical-align: top
+                margin-top: 21px 
+
+</style>
---
+++ pages/search/search.vue
@@ -0,0 +1,64 @@
+<template>
+	<div class="search">
+		<div class="container goodsListBox">
+			<div class="goodsList">
+				<div class="searchNav">
+					
+				</div>
+				<div class="item">
+					<rank />
+				</div>
+				<goodsItem :goodsItemStyleObj="goodsItemStyleObj" />
+				<div class="changePage">
+					<el-pagination
+					  	background
+					  	layout="prev, pager, next"
+					  	:total="300">
+					</el-pagination>
+				</div>
+			</div>
+			<moreGoods />
+		</div>
+	</div>
+</template>
+<script>
+	import moreGoods from "~/components/base/moreGoods"
+	import rank from "~/components/base/rank"
+	import goodsItem from "~/components/base/goodsItem"
+	export default {
+		layout: 'searchHome',
+		data() {
+			return {
+				goodsItemStyleObj: {
+					width: '172px',
+					height: '275px'
+				}
+			}
+		},
+		components: {
+			moreGoods,
+			rank,
+			goodsItem
+		},
+		mounted() {
+
+			
+		},
+	  	computed: {  
+		    
+	  	},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang='sass' scoped>
+	@import '~/assets/sass/common.sass'
+	.search
+		.goodsListBox 
+			overflow: hidden
+			.goodsList
+				float: left
+				width: 942px
+</style>
